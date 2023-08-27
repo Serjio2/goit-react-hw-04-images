de593f4aa3552d71f5612b762de90aabde129e0a
Serhii Kondratenko <kondratenko.serhii3@gmail.com>
2023-08-27 21:22:57 +0300

    finish 04- image

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { GlobalStyle } from './GlobalStyle';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { Searchbar } from './Searchbar/Searchbar';
@@ -6,65 +6,122 @@ import { ButtonLoadMore } from './Button/Button';
 import { fetchImages } from './api';
 import { Loader } from './Loader/Loader';
 
-export class App extends Component {
-  state = {
-    query: '',
-    images: [],
-    page: 1,
-    loading: false,
-    error: null,
-  };
+export const App = () => {
+  const [query, setQuery] = useState('');
+  const [images, setimages] = useState([]);
+  const [page, setpage] = useState(1);
+  const [loading, setloading] = useState(false);
+  const [error, seterror] = useState(false);
 
-  handleChangeQuery = newQuery => {
-    this.setState({
-      query: `${Date.now()}/${newQuery}`,
-      images: [],
-      page: 1,
-    });
-  };
-
-  async componentDidUpdate(prevProps, prevState) {
-    try {
-      const { query, page } = this.state;
-      if (prevState.query !== query || prevState.page !== page) {
-        this.setState({ loading: true });
-
+  useEffect(() => {
+    seterror(false);
+    async function fetchData() {
+      try {
+        setloading(true);
+        if (query === '') {
+          return;
+        }
         const data = await fetchImages(
           query.slice(query.indexOf('/') + 1),
           page
         );
 
-        this.setState(prevState => {
-          return {
-            images: [...prevState.images, ...data],
-            loading: false,
-          };
-        });
+        setimages(prevState => [...prevState, ...data]);
+      } catch (error) {
+        seterror(true);
+      } finally {
+        setloading(false);
       }
-    } catch (error) {
-      this.setState({ error });
     }
-  }
+    fetchData();
+  }, [query, page]);
 
-  handleLoadMore = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
+  const handleChangeQuery = newQuery => {
+    setQuery(`${Date.now()}/${newQuery}`);
+    setimages([]);
+    setpage(1);
   };
 
-  render() {
-    const { error, images, loading } = this.state;
-    return (
-      <>
-        <GlobalStyle />
-        <Searchbar onSubmit={this.handleChangeQuery} />
-        {error && <p>Ooops, somthing wrong: {error.message}. Reload page.</p>}
-        {images.length > 0 && <ImageGallery images={images} />}
-        {loading && <Loader />}
-        {images.length > 0 && !loading && (
-          <ButtonLoadMore onClick={this.handleLoadMore} />
-        )}
-      </>
-    );
-  }
-}
+  const handleLoadMore = () => {
+    setpage(prevState => prevState + 1);
+  };
+
+  return (
+    <>
+      <GlobalStyle />
+      <Searchbar onSubmit={handleChangeQuery} />
+      {error && <p>Ooops, somthing wrong. Reload page.</p>}
+      {images.length > 0 && <ImageGallery images={images} />}
+      {loading && <Loader />}
+      {images.length > 0 && !loading && (
+        <ButtonLoadMore onClick={handleLoadMore} />
+      )}
+    </>
+  );
+};
+
+// *****************************
+// use class
+
+// export class App extends Component {
+//   state = {
+//     query: '',
+//     images: [],
+//     page: 1,
+//     loading: false,
+//     error: null,
+//   };
+
+//   handleChangeQuery = newQuery => {
+//     this.setState({
+//       query: `${Date.now()}/${newQuery}`,
+//       images: [],
+//       page: 1,
+//     });
+//   };
+
+//   async componentDidUpdate(prevProps, prevState) {
+//     try {
+//       const { query, page } = this.state;
+//       if (prevState.query !== query || prevState.page !== page) {
+//         this.setState({ loading: true });
+
+//         const data = await fetchImages(
+//           query.slice(query.indexOf('/') + 1),
+//           page
+//         );
+
+//         this.setState(prevState => {
+//           return {
+//             images: [...prevState.images, ...data],
+//             loading: false,
+//           };
+//         });
+//       }
+//     } catch (error) {
+//       this.setState({ error });
+//     }
+//   }
+
+//   handleLoadMore = () => {
+//     this.setState(prevState => ({
+//       page: prevState.page + 1,
+//     }));
+//   };
+
+//   render() {
+//     const { error, images, loading } = this.state;
+//     return (
+//       <>
+//         <GlobalStyle />
+//         <Searchbar onSubmit={this.handleChangeQuery} />
+//         {error && <p>Ooops, somthing wrong: {error.message}. Reload page.</p>}
+//         {images.length > 0 && <ImageGallery images={images} />}
+//         {loading && <Loader />}
+//         {images.length > 0 && !loading && (
+//           <ButtonLoadMore onClick={this.handleLoadMore} />
+//         )}
+//       </>
+//     );
+//   }
+// }
